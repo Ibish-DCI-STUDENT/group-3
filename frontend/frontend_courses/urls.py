191f5a707c3e93a45a578551033814d70e1eb7c8
Ibish <ibish.selmani@dci-student.org>
2024-02-22 22:13:09 +0100

    Homepage

--- frontend/frontend_courses/urls.py
+++ frontend/frontend_courses/urls.py
@@ -4,7 +4,7 @@ from .views import HomeView, CourseList, DetailView
 app_name = "frontend_courses"  # This is the namespace, needed in urls.py of it_courses
 
 urlpatterns = [
-    path("home/", HomeView.as_view(), name="home"),
+    path("", HomeView.as_view(), name="home"),
     path("course_list/", CourseList.as_view(), name="course_list"),
     path("course_detail/<int:course_id>/", DetailView.as_view(), name="course_detail"),
     
