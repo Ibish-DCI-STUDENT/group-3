f684565683e6dc1bfcf0fe5e5dfb14770b92ed68
Ibish <ibish.selmani@dci-student.org>
2024-03-02 22:12:57 +0100

    just

--- frontend/accounts/models.py
+++ frontend/accounts/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class CustomUser(AbstractUser):
     name = models.CharField(null=True, blank=True, max_length=100)
     purchased_courses = models.JSONField(default=list)
+    
+    
+    
