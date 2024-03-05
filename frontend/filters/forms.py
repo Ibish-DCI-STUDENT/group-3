d3191c883470a773e5361bdc52a3c3560cee6339
Lon-B <lon.baecker@dci-student.org>
2024-03-05 22:02:48 +0100

    final css touches

--- frontend/filters/forms.py
+++
@@ -1,9 +0,0 @@
-# filters/forms.py
-
-from django import forms
-
-class FilterForm(forms.Form):
-    min_price = forms.DecimalField(required=False)
-    max_price = forms.DecimalField(required=False)
-    instructor = forms.CharField(required=False)
-    duration = forms.CharField(required=False)
